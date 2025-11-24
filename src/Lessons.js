db2d43fa4506370340d144ea993591aa153c42eb
Morgan <morganpstanley@gmail.com>
2025-11-23 20:47:53 -0700

    Update Lessons.js

--- src/Lessons.js
+++ src/Lessons.js
@@ -4,19 +4,18 @@ import bugSearching from "./assets/bug-searching.webp"
 const Lessons = ({lessonsRef, contactRef}) => {
 
   const jumpToRef = () => {
-    console.log('hit me')
     contactRef.current.scrollIntoView({ behavior: "smooth" });
   }
 
   return (
     <div ref={lessonsRef} className="block" id="lessons" >
       <h1>🎶Lessons</h1>
-      <p>I am currently on faculty at Weber State University and Gifted Music School in Salt Lake City, Utah. 
+      <p>I am currently on faculty at Weber State University, Brigham Young University, and the Gifted Music School in Salt Lake City, Utah. 
         I also maintain an active private guitar studio in the Salt Lake area. I teach both classical 
-        and fingerstyle guitar, as well as ukulele and ensemble classes. Lessons are offered Tuesday-Thursday.
+        and fingerstyle guitar, as well as ukulele and ensemble classes. Private lessons are offered Wednesday-Thursday.
       </p>       
       <p><button onClick={jumpToRef}>Contact me</button> for more information.</p>
-      <b>Rate: $35 per half-hour lesson</b>
+      <b>Rate: $40 per half-hour lesson</b>
 
       <br />
       <img src={bugSearching} alt="" id="bug-photo"/>
